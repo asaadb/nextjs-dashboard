07a4d2e76440ee1a1d676253e0adb0163486f094
Asaad Budayr <asad_289@hotmail.com>
2023-02-24 16:42:28 +0300

    feat: create header

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import Header from "../components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +14,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className=""></main>
+      <main className="bg-gray-100 min-h-screen">
+        <Header />
+      </main>
     </>
   );
 }
